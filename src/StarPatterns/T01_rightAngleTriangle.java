7ae2e32539b9120de1f4f8e3a68995ba99ebfc6a
S.Zahir <zshunay@gmail.com>
2023-11-11 17:03:52 -0500

    task list, git ignore committed

---
+++ src/StarPatterns/T01_rightAngleTriangle.java
@@ -0,0 +1,38 @@
+package StarPatterns;
+
+public class T01_rightAngleTriangle {
+    public static void main(String[] args) {
+     int num=7;
+
+        for (int i = 1; i <=num ; i++) { //rows
+            for (int j = 1; j <=i ; j++) { //columns
+
+                //System.out.print(i + " ");
+               // System.out.print("* ");
+                System.out.print(i );
+
+
+            }
+            System.out.println();
+
+        }
+
+
+
+
+
+
+    }
+
+}
+/*
+1. Write a program that takes X number from the user and prints out right-angled triangle pattern.
+
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+6 6 6 6 6 6
+7 7 7 7 7 7 7
+ */

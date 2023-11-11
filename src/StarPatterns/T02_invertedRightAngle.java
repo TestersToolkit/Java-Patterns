75cbcbfef71ce322b03d514b7249cb1163b8acfa
S.Zahir <zshunay@gmail.com>
2023-11-11 18:48:28 -0500

    inverse right angle triangle

---
+++ src/StarPatterns/T02_invertedRightAngle.java
@@ -0,0 +1,23 @@
+package StarPatterns;
+
+public class T02_invertedRightAngle {
+    public static void main(String[] args) {
+
+        int num=7;
+
+        for (int i = num; i >=1 ; i--) { //rows
+            for (int j = 1; j <=i ; j++) { //columns
+               // System.out.print(i + " ");
+                System.out.print("* ");
+
+
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+ Print an inverted right-angled triangle pattern of stars.
+
+ */
+

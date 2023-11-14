49431f88ff511b6189d4b48b2d853468b580855a
S.Zahir <zshunay@gmail.com>
2023-11-14 14:51:17 -0500

    Task 9 updated

--- src/StarPatterns/T01_rightAngleTriangle.java
+++ src/StarPatterns/T01_rightAngleTriangle.java
@@ -6,7 +6,7 @@ public class T01_rightAngleTriangle {
 
         for (int i = 1; i <=7 ; i++) { //rows
             for (int j = 1; j <=i ; j++) { //columns
-                System.out.print(i + " ");
+                System.out.print( i + " ");
 
             }
             System.out.println();
